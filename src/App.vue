78fb170e8c15f73068528b09da6303e48277d5ed
Liujia <Liujia@LAPTOP-1U6MOFGU>
2021-09-08 22:27:45 +0800

    底部路由实现

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-   
+    <router-view></router-view>
+   <common-tab-bar></common-tab-bar>
   </div>
 </template>
 
 <script>
-
+import CommonTabBar from 'components/business/commontabbar/CommonTabBar'
 export default {
   name: 'App',
   components: {
+    CommonTabBar
   }
 }
 </script>
