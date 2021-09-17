59d8217faa190a8270d820248ed49f8db81c9987
Liujia <Liujia@LAPTOP-1U6MOFGU>
2021-09-17 11:34:04 +0800

    首页tab切换封装

--- src/network/home.js
+++ src/network/home.js
@@ -4,3 +4,14 @@ export function getHomeDataList () {
     url: '/home/multidata'
   })
 }  
+
+
+export function getHomeGoodsList (type, page) {
+  return request({
+    url: '/home/data',
+    params: {
+      type,
+      page
+    }
+  })
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,41 +4,109 @@
       <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
       <home-swiper :banners="banners"></home-swiper>
       <home-recommend :recommends="recommends"></home-recommend>
+      <feature-view></feature-view>
+      <tab-control :titles="titles" class="tab-control" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
     </div>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './homeChild/HomeSwiper'
 import HomeRecommend from './homeChild/HomeRecommend'
-import { getHomeDataList } from 'network/home'
+import FeatureView from './homeChild/FeatureView.vue'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/business/tabControl/TabControl'
+import GoodsList from 'components/business/goods/GoodsList'
+
+
+import { getHomeDataList, getHomeGoodsList } from 'network/home'
 export default {
   name: 'Home',
   components: {
-    NavBar,
     HomeSwiper,
     HomeRecommend,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodsList,
   },
   created () {
-    // 获取数据
-    getHomeDataList().then(res => {
-      this.banners = res.data.banner.list
-      this.recommends = res.data.recommend.list;
-    })
+    this.getHomeDataList()
+    this.getHomeGoodsList('pop')
+    this.getHomeGoodsList('new')
+    this.getHomeGoodsList('sell')
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
+    }
+  },
+  methods: {
+     // 获取数据
+    getHomeDataList () {
+      getHomeDataList().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    // 获取商品数据
+    getHomeGoodsList(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoodsList(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1;
+      })
+    },
+    // 子组件传递
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+      }
+    }
   },
   data () {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      titles: [{id: 1,type: '流行'},{id: 2,type: '新款'},{id: 3,type: '精选'},],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      },
+      currentType: 'pop',
     }
   }
 }
 </script>
 
 <style>
+  #home {
+   padding-top: 44px; 
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    left: 0;
+    top:0;
+    right: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
   }
 </style>
